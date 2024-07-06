ed7633f6e2c7f3a70a4cb959324946f0c83b52c5
Axel Yoliston Vasquez Herrera <axelvasquezherrera@outlook.com>
2024-07-06 00:20:59 -0500

    avance

--- udemy/4_calculadora_propinas/src/App.tsx
+++ udemy/4_calculadora_propinas/src/App.tsx
@@ -1,10 +1,11 @@
 import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
 
-  const { addItem } = useOrder()
+  const { order, addItem } = useOrder()
 
   return (
     <>
@@ -23,11 +24,14 @@ function App() {
             ))
           }
           </div>
+        </div>
 
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents
+            order={order}
+          />
         </div>
-        <div>
-          <h2>Consumo</h2>
-        </div>
+
       </main>
     </>
   );
